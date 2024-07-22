8ad30cec6cd615333025f49bcda40406ba98547a
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-21 19:25:44 -0700

    fixed hover link bug

--- src/pages/frontpage.jsx
+++ src/pages/frontpage.jsx
@@ -3,11 +3,16 @@ import { createAuthLink } from "../utils/createAuthLink";
 export default function Frontpage() {
 
   const authLink = createAuthLink();
-  console.log(authLink);
 
   return (
     <div className="frontpage-container">
-      <h1 className="frontpage-login"><a href={authLink} className="frontpage-login">login</a></h1>
+      <div className="frontpage-content">
+        <h1 className="frontpage-login">
+          <a href={authLink} className="frontpage-login">log in</a>
+        </h1>
+        <p>and click on one of your playlists to add all of its songs to your saved songs</p>
+      </div>
+      <p className="frontpage-footer">Batch music save utility by Ramit Brata Biswas (https://github.com/ramitbratabiswas), 2024</p>
     </div>
   );
 }
--- src/pages/utility.jsx
+++ src/pages/utility.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-
 import { useBatchSave } from "../utils/batchSave";
 import { useFetchMyPlaylists } from "../utils/fetchMyPlaylists";
 
 export default function Utility() {
-
   const { playlists, owners } = useFetchMyPlaylists();
   const [playlistToSave, setPlaylistToSave] = useState("");
   const [ownerFilter, setOwnerFilter] = useState("all");
@@ -13,22 +11,32 @@ export default function Utility() {
 
   const handleClickPlaylist = (id) => {
     setPlaylistToSave(id);
-  }
+  };
 
   const handleClickOwner = (owner) => {
     setOwnerFilter(owner);
-  }
+  };
 
   return (
     <div className="utility-container">
-      <h1 className="utility-header">your playlists:</h1>
-      <ul>
-      {playlists.filter((item) => (ownerFilter === "all" || item.owner === ownerFilter)).map((item, index) => <li onClick={() => handleClickPlaylist(item.id)} key={index+1}>{index+1}. <span>{item.name} {(ownerFilter === "all") ? ` - ${item.owner}` : ""}</span></li>)} 
+      <p className="owners-header">filter by owner: {ownerFilter}</p>
+      <ul className="owners">
+        {owners.map((item, index) => (
+          <li className="owner-name" key={index + 1}>
+            {index + 1}. <span onClick={() => handleClickOwner(item)}>{item}</span>
+          </li>
+        ))}
       </ul>
-      <br/>
-      <h1 className="utility-owners">filter by owner: {ownerFilter}</h1>
-      <ul>
-      {owners.map((item, index) => <li onClick={() => handleClickOwner(item)} key={index+1}>{index+1}. <span>{item}</span></li>)}
+      <br />
+      <h1 className="playlists-header">your playlists:</h1>
+      <ul className="playlists">
+        {playlists
+          .filter((item) => ownerFilter === "all" || item.owner === ownerFilter)
+          .map((item, index) => (
+            <li className="playlist-name" key={index + 1}>
+              {index + 1}. <span onClick={() => handleClickPlaylist(item.id)}>{item.name} {ownerFilter === "all" ? ` - ${item.owner}` : ""}</span>
+            </li>
+          ))}
       </ul>
     </div>
   );
